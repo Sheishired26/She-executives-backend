46600e7a755b8dc46d9f84a0a74746125f3105e5
Harsh Mekhiya <harshmekhiya191@gmail.com>
2026-03-22 11:34:41 +0530

    allow localhost 8080

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "http://localhost:8080", // 👈 ADD THIS
       "https://she-executives.netlify.app",
     ],
     methods: ["GET", "POST"],
